385628e66d7c0d787074702fffb43caad435d1a9
bnguyen654 <bnguyen9469@yahoo.com>
2014-06-19 21:36:26 -0700

    Header and Bootstrap scripts.

---
+++ header.php
@@ -0,0 +1,38 @@
+<link href="styles/bootstrap.css" rel="stylesheet">
+<link href="styles/bootswatch.css" rel="stylesheet"/>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="config/bootstrap.js"></script>
+
+<div class="navbar navbar-default">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Simple Studios</a>
+  </div>
+  <div class="navbar-collapse collapse navbar-responsive-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Active</a></li>
+      <li><a href="#">Link</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li class="dropdown-header">Dropdown header</li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+      <ul class="nav nav-pills navbar-right">
+      <li><a href="#">Profile <span class="badge"></span></a></li>
+      <li><a href="#">Messages <span class="badge">0</span></a></li>
+    </ul>
+
+  </div>
+</div>
